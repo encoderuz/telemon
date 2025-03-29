279913a448bedd06fcb029fed44bb820b7eee45d
encoderuz <abduqodirxojiyev@gmail.com>
2025-03-30 04:17:48 +0500

    parse mode added to config

--- src/client/client.rs
+++ src/client/client.rs
@@ -12,6 +12,7 @@ struct SendMessagePayload<'a> {
     chat_id: i64,
     text: &'a str,
     message_thread_id: i64,
+    parse_mode: &'a str,
 }
 
 impl TelegramClient {
@@ -21,7 +22,18 @@ impl TelegramClient {
             token: CONFIG.token.clone(),
         }
     }
+    pub fn escape_markdown_v2(text: &str) -> String {
+        let special_chars = r"_*[]()~`>#+-=|{}.!";
 
+        let mut escaped = String::with_capacity(text.len());
+        for c in text.chars() {
+            if special_chars.contains(c) {
+                escaped.push('\\');
+            }
+            escaped.push(c);
+        }
+        escaped
+    }
     pub fn send_message(
         &self,
         chat_id: i64,
@@ -29,11 +41,11 @@ impl TelegramClient {
         message: &str,
     ) -> Result<(), Box<dyn std::error::Error>> {
         let url = format!("https://api.telegram.org/bot{}/sendMessage", self.token);
-
         let payload = SendMessagePayload {
             chat_id,
             text: message,
             message_thread_id: thread_id,
+            parse_mode: &CONFIG.parse_mode,
         };
 
         let res = self.client.post(&url).json(&payload).send()?;
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,5 @@ use telemon::Telemon;
 
 fn main() {
     // chat_id configda mavjud bo‘lsa
-    Telemon::send("✅ Success").to(34);
-
-    // configda chat_id yo‘q bo‘lsa
-    Telemon::send("🚨 Xatolik").to((-1002483629475, 34));
+    Telemon::send("✅ Success ```telemon```").to(2);
 }
